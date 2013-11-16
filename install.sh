58de87fa71fec490a0e577cc3effe65e3d5b36e9
Mario Ricalde <kuroir@gmail.com>
2013-11-16 02:18:00 -0600

    Punctuation please.

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if [ ! -d "$HOME/.magus" ]; then
-    echo "Installing Magus for the first time"
+    echo "Installing Magus for the first time."
     git clone https://github.com/hybridgroup/magus.git "$HOME/.magus"
     cd "$HOME/.magus"
     [ "$1" == "ask" ] && export ASK="true"
     rake install
 else
-    echo "Magus is already installed"
+    echo "Magus is already installed."
 fi
