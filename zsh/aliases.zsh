26013860d416277b85696b7fdc78f7ce80089e4e
Mario Ricalde <kuroir@gmail.com>
2014-03-15 05:38:12 -0600

    We want groups for ls

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -18,7 +18,6 @@ alias psr='ps aux | grep ruby'
 # Show human friendly numbers and colors
 alias df='df -h'
 alias ll='ls -alGh'
-alias ls='ls -Gh'
 alias du='du -h -d 2'
 
 # show me files matching "ls grep"
